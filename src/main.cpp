39e2043dd631aeb61af781ee4878eac0c6447bf8
Administrator <Administrator@WIN-NR57M63DD8T>
2021-07-30 23:55:29 +0200

    Add example usage to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <Windows.h>
 #include "utils/c_log.h"
+#include "c_injector.h"
 
 int main() {
     utils::g_log->set_console_handle(GetStdHandle(STD_OUTPUT_HANDLE));
 
     try {
-
+        c_injector injector{};
+        injector.inject("moh_spearhead.exe","./internal_template.dll", true);
     } catch (const std::exception& e) {
         utils::g_log->warn(e.what());
     }
