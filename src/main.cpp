828cf3d8c740510e53ef8e2dc5cb34d53d88b730
Administrator <Administrator@WIN-NR57M63DD8T>
2021-07-31 00:02:12 +0200

    Add argument support and comment out info logs

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,17 @@
 #include "utils/c_log.h"
 #include "c_injector.h"
 
-int main() {
+int main(int argc, char** argv) {
     utils::g_log->set_console_handle(GetStdHandle(STD_OUTPUT_HANDLE));
+    if (argc < 2) {
+        utils::g_log->warn("usage: program.exe <process name> <dll name>");
+        getchar();
+        return 1;
+    }
 
     try {
         c_injector injector{};
-        injector.inject("moh_spearhead.exe","./internal_template.dll", true);
+        injector.inject(argv[1], argv[2], true);
     } catch (const std::exception& e) {
         utils::g_log->warn(e.what());
     }
